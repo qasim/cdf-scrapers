10a4a11252fe2dd38ef7a894bdeab6067569e56a
Eugene Cheung <ey.cheung@outlook.com>
2015-04-06 14:33:53 -0400

    Remove some redundancy

--- lpq.py
+++ lpq.py
@@ -4,10 +4,10 @@ import datetime
 import subprocess
 
 def getData(printer):
-    """Return the data from calling lpq with the specified printer."""
+    """Return the data from calling lpq with the specified printer as a list."""
     cmd = 'lpq -P' + printer
     data = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read()
-    return(data.decode('utf-8'))
+    return(data.decode('utf-8').split('\n')
 
 def parseData(list):
     """Returns the list without the header lines."""
@@ -15,9 +15,9 @@ def parseData(list):
 
 if __name__ == '__main__':
     # Gets all the data
-    p2210a = getData('p2210a').split('\n')
-    p2210b = getData('p2210b').split('\n')
-    p3185a = getData('p3185a').split('\n')
+    p2210a = getData('p2210a')
+    p2210b = getData('p2210b')
+    p3185a = getData('p3185a')
 
     data = {}
 
@@ -30,7 +30,4 @@ if __name__ == '__main__':
     data['2210b'] = parseData(p2210b)
     data['3185a'] = parseData(p3185a)
 
-    json_data = json.dumps(data)
-
-    print(json_data)
-
+    print(json.dumps(data))
