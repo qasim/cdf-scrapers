2dce8a662c9e067d260622ae73ac55844068dbd4
Eugene Cheung <ey.cheung@outlook.com>
2015-10-10 17:07:30 -0400

    Change file name

--- lpq.sh
+++ lpq.sh
@@ -6,7 +6,7 @@ rm -f "$home/public_html/printdata.json"
 /local/bin/python3 "$home/cdf-printdata/lpq.py" > "$home/public_html/printdata.json"
 chmod og+r "$home/public_html/printdata.json"
 
-rm -f "$home/public_html/printdata_all.json"
-/local/bin/python3 "$home/cdf-printdata/lpqa.py" > "$home/public_html/printdata_all.json"
-chmod og+r "$home/public_html/printdata_all.json"
+rm -f "$home/public_html/cdfprinters.json"
+/local/bin/python3 "$home/cdf-printdata/lpqa.py" > "$home/public_html/cdfprinters.json"
+chmod og+r "$home/public_html/cdfprinters.json"
 
