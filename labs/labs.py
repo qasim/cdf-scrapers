545ca86a9e5e21bbb06c24bdcef6e15492095cec
Eugene Cheung <ey.cheung@outlook.com>
2016-01-04 19:25:57 -0500

    Fetch lab data/parsing demo without external libraries

--- labs/labs.py
+++ labs/labs.py
@@ -1,20 +1,14 @@
-# http://docs.python-guide.org/en/latest/scenarios/scrape/
-from lxml import html
-import requests
+from html.parser import HTMLParser
+import urllib.request
 
-page = requests.get('http://www.cdf.toronto.edu/usage/usage.html')
-tree = html.fromstring(page.content)
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Encountered a start tag:", tag)
+    def handle_endtag(self, tag):
+        print("Encountered an end tag :", tag)
+    def handle_data(self, data):
+        print("Encountered some data  :", data)
 
-names      = tree.xpath('//table/tr[position() > 1]/td[1]/text()')
-avail      = tree.xpath('//table/tr[position() > 1]/td[2]/text()')
-busy       = tree.xpath('//table/tr[position() > 1]/td[3]/text()')
-total      = tree.xpath('//table/tr[position() > 1]/td[4]/text()')
-percentage = tree.xpath('//table/tr[position() > 1]/td[5]/text()')
-time       = tree.xpath('//table/tr[position() > 1]/td[6]/text()')
-
-print 'Names: ', names
-print 'Avail: ', avail
-print 'Busy:  ', busy
-print 'Total: ', total
-print '%:     ', percentage
-print 'Time:  ', time
+html = str(urllib.request.urlopen('http://www.cdf.toronto.edu/usage/usage.html').read())
+parser = MyHTMLParser()
+parser.feed(html)
