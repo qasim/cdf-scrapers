bf5c3f2b209be58208433fbcc343f3cbb2301564
Eugene Cheung <ey.cheung@outlook.com>
2016-01-13 19:23:50 -0500

    Fix labs timestamp

--- labs/labs.py
+++ labs/labs.py
@@ -49,8 +49,8 @@ class PageParser(HTMLParser):
 
             elif self.row_cell == 5:
                 if (self.timestamp == ''):
-                    time.strptime(data.strip('\u00a0\\n'), '%a %b %d %H:%M:%S EST %Y')
-                    self.timestamp = time.strftime('%Y-%m-%d %H:%M:%S EST')
+                    timestamp = time.strptime(data.strip('\u00a0\\n'), '%a %b %d %H:%M:%S EST %Y')
+                    self.timestamp = time.strftime('%Y-%m-%d %H:%M:%S EST', timestamp)
 
                 self.row_cell = -1
 
